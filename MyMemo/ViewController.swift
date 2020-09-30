2f24d6b75c20d40ca520a7edc4295a55b2be2680
takroid <sheetake1204@gmail.com>
2020-09-30 21:05:24 +0900

    commit for remote

---
+++ MyMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemo
+//
+//  Created by 金澤武士 on 2020/09/30.
+//  Copyright © 2020 tk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
