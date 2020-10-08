1f42e321bcc26adc016cff8da17c4111517eb2ae
takroid <sheetake1204@gmail.com>
2020-10-08 23:17:29 +0900

    tableviewのデリゲート設定

--- MyMemo/ViewController.swift
+++ MyMemo/ViewController.swift
@@ -6,11 +6,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //セル数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    //セル毎の情報
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+    //セクション数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    //セクションのタイトル
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        <#code#>
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
