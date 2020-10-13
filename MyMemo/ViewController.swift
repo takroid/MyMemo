4e1ce4148e17f9729d211e0af30acb7eabf35fb0
takroid <sheetake1204@gmail.com>
2020-10-13 22:04:24 +0900

    tableview追記

--- MyMemo/ViewController.swift
+++ MyMemo/ViewController.swift
@@ -7,30 +7,44 @@
 import UIKit
 import RealmSwift
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UIGestureRecognizerDelegate {
 
+//    //画面タッチでキーボード閉じる
+//    @objc func tapped(_ sender: UITapGestureRecognizer) {
+//        if sender.state == .ended {
+//            textField.resignFirstResponder()
+//        }
+//    }
     //セル数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       <#code#>
+       return 1
     }
     //セル毎の情報
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        //カスタムセルを使用
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskMemoCell")
+        return cell!
     }
 
     //セクション数
     func numberOfSections(in tableView: UITableView) -> Int {
-        <#code#>
+        return 1
     }
     //セクションのタイトル
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        <#code#>
+        //
     }
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        //タップでキーボード閉じる
+//        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(
+//            target: self,
+//            action: #selector(ViewController.tapped(_:)))
+//        //tableviewへのtapを検知させる
+//        tapGesture.cancelsTouchesInView = false
     }
 
 
